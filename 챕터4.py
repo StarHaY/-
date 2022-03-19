1b4cbe8939c3c3a9aa6027066f9037479e348ee2
StarHaY <101918579+StarHaY@users.noreply.github.com>
2022-03-19 15:09:33 +0900

    Add files via upload

---
+++ 챕터4.py
@@ -0,0 +1,22 @@
+#반복문
+#리스트 반복하기
+characters=['앨리스','도도새','3월 토끼']
+for i in characters:
+    print(i)
+
+print('===============================')
+#문자열 반복하기
+for i in '문자열 반복하기':
+    print(i)
+    
+print('===============================')
+#순서열 만들기
+for num in range(3):
+    print(num)
+    
+print('===============================')
+#구구단
+for i in range(2,10):
+    for j in range(1,10):
+        print(i,'X',j,'=',i*j)
+        
